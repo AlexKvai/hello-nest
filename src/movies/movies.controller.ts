95990d6b43c19303618dd219e2e65d488b7827c6
alex parfenov <alexp-2004@mail.ru>
2022-03-06 02:07:54 +0700

    Изменения контроллера

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,30 @@
 import {Controller, Delete, Get, Post, Param, Patch, Body, Query} from '@nestjs/common';
+import {MoviesService} from "./movies.service";
+import {Movie} from "./entities/movie.entity";
 
 @Controller('movies')
 export class MoviesController {
-
-    @Get()
-    getAll() {
-        return "Здесь будут возвращаться все фильмы";
+    constructor(private readonly moviesService: MoviesService) {
     }
 
-    @Get('search')
-    search(@Query('year') searchingYear : string){
-        return `Мы ищем фильм, выпущенный после ${searchingYear} года`;
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
     @Get(':id')
-    getOne(@Param('id') movieId : string){
-        return `Здесь будет возвращаться один фильм с айдишником ${movieId}` ;
+    getOne(@Param('id') movieId : string): Movie{
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete(':id')
     remove(@Param('id') movieId : string) {
-        return `Эта функция будет удалять фильм с айдишником ${movieId}`;
+        return this.moviesService.remove(movieId);
     }
 
     @Patch(':id')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -3,5 +3,25 @@ import {Movie} from "./entities/movie.entity";
 
 @Injectable()
 export class MoviesService {
-    public movies: Movie[] = [];
+    private movies: Movie[] = [];
+    getAll(): Movie[] {
+        return  this.movies;
+    }
+
+    getOne(id: string) : Movie {
+        return this.movies.find(movie => movie.id === parseInt(id));
+    }
+
+    remove(id: string) : Boolean {
+        this.movies.filter((movie) => movie.id !== parseInt(id));
+        return true;
+    }
+
+    create(movieData) {
+        this.movies.push({
+            id: this.movies.length + 1,
+            ...movieData,
+            }
+        );
+    }
 }
