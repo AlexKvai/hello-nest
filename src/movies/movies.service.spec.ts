0730c4551b691a8375916eb64de6173971fddc33
alex parfenov <alexp-2004@mail.ru>
2022-03-08 23:06:25 +0700

    Добавлены новые unit тесты

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -39,7 +39,7 @@ describe('MoviesService', () => {
       expect(movie.id).toEqual(1);
     });
 
-    it('Должна возвращаться 404 ошибка', () => {
+    it('Должна возвращаться ошибка NotFoundException', () => {
       try {
         service.getOne(1234);
       }
@@ -48,6 +48,67 @@ describe('MoviesService', () => {
         expect(e.message).toEqual(`Фильм с id: 1234 не найден`);
       }
     })
+  })
+
+  describe('Тестирование функции remove', () => {
+    it('Фильм удаляется', () => {
+      service.create({
+        title: "Тестовый фильм",
+        genres: ["Тестовый жанр"],
+        year: 2000,
+      });
+      const allMovies = service.getAll();
+      service.remove(1);
+      const afterRemove = service.getAll();
+      expect(afterRemove.length).toEqual(allMovies.length - 1);
+    });
+
+    it('Должна возвращаться ошибка NotFoundException', () => {
+      try {
+        service.remove(1234);
+      }
+      catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  })
+
+  describe('Тестирование функции create', () => {
+    it('Фильм создаётся', () => {
+
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: "Тестовый фильм",
+        genres: ["Тестовый жанр"],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  })
+
+  describe('Тестирование функции patch', () => {
+    it('Фильм изменён', () => {
+      service.create({
+        title: "Тестовый фильм",
+        genres: ["Тестовый жанр"],
+        year: 2000,
+      });
+      service.patch(1, {title: 'Обновлённый тест'});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Обновлённый тест');
+    });
+
+    it('Должна возвращаться ошибка NotFoundException', () => {
+      try {
+        service.patch(1234, {title: ''});
+      }
+      catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+
   })
 
 });
